2a52068d8df684090bc69fee6ca4b26629a3f907
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-07-02 06:54:24 +0530

    #22, #17 - index has been tested.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
   "use strict";
 
   $(function(){
-
     var dimensionEvents = function(){
         $('.dimension-options button').on('click', function(){
           $('.dimension-options button').removeClass('btn-success');
@@ -61,9 +60,79 @@
       init = function(){
         $('#600x400, #landscape').trigger('click');
         $('.top-taker').TopTaker({'theme': 'dark'});
-      };
+      },
+      tests = function(){
+        if(window.location.href === "file:///D:/projext/jspaint/src/index.html"){
+          mocha.setup('bdd');
+          mocha.reporter('html');
 
-      setup();
-      init();
+          var assert = chai.assert;
+
+          describe('Testing.', function(){
+            describe('Testing dimension events, as in, do objects have right events attached to them?', function(){
+              var opt = $('.dimension-options button');
+              var events = $._data(opt[0], "events");
+              it("'.dimension-options button' should have a defined click event attached to it.", function(){
+                assert(events.click !== "undefined");
+              });
+              it("'.dimension-options button' should have exactly one defined click event attached to it.", function(){
+                assert(events.click.length === 1);
+              });
+            });
+            describe('Testing orientation events, as in, do objects have right events attached to them?', function(){
+              var opt = $('.orientation-options button');
+              var events = $._data(opt[0], "events");
+              it("'.orientation-options button' should have a defined click event attached to it.", function(){
+                assert(events.click !== "undefined");
+              });
+              it("'.orientation-options button' should have exactly one defined click event attached to it.", function(){
+                assert(events.click.length === 1);
+              });
+            });
+            describe('Testing jspaint-action events, as in, do objects have right events attached to them?', function(){
+              var action = $('#jspaint-action');
+              var events = $._data(action[0], "events");
+              it("'#jspaint-action' should have a defined click event attached to it.", function(){
+                assert(events.click !== "undefined");
+              });
+              it("'#jspaint-action' should have exactly one defined click event attached to it.", function(){
+                assert(events.click.length === 1);
+              });
+            });
+            describe("'Let\'s paint' action button is being tested.", function(){
+              var action = $('#jspaint-action');
+              var originalHref = action.attr('href');
+              var newHref = './jspaint.html?dimension=600x400';
+              it("should have './jspaint.html' as original value for href.", function(){
+                assert(originalHref === './jspaint.html');
+              });
+              it("should generate uri = " + newHref + " by default", function(){
+                var uriObtained = newUri(action);
+                assert(uriObtained = newHref);
+              });
+            });
+            describe("Default options are selected.", function(){
+              var defaultDimension = "600x400",
+                  defaultOrientation = "landscape";
+
+              it("should check that default dimension is " + defaultDimension, function(){
+                assert($('.dimension-options button.btn-success').attr('id') === defaultDimension);
+              });
+              it("should check that default orientation is " + defaultOrientation, function(){
+                assert($('.orientation-options button.btn-success').attr('id') === defaultOrientation);
+              });
+            });
+          });
+        }
+        if (navigator.userAgent.indexOf('PhantomJS') < 0) {
+          mocha.run();
+        }
+      },
+      mustRunInSequence = function(){
+        setup();
+        init();
+        tests();
+      };
+      mustRunInSequence();
   });
 })(jQuery);

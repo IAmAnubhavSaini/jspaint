58de0989cdc20c31c63705c255d050341a2948cf
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-07-06 02:54:20 +0530

    Closes #52, closes #53, closes #54, closes #55, closes #56

--- src/jspaint.js
+++ src/jspaint.js
@@ -75,6 +75,13 @@
             },
             fillSquare: function (x, y, side) {
                 context.fillRect(x, y, side, side);
+            },
+            fillRing: function (options) {
+                CANVASAPI.fillCirc(options.X, options.Y, options.outerRadius);
+                context.save();
+                context.fillStyle = resetCanvasColor;
+                CANVASAPI.fillCirc(options.X, options.Y, options.innerRadius);
+                context.restore();
             }
         };
         var Color = {
@@ -441,7 +448,7 @@
                 ContextMenu: {
                     activate: function (options) {
                         function initialSlider() {
-                            return $('<input id="radiusDisc" type="range" min="1" max="100" step="1" title="width for pencil tool." />');
+                            return $('<input id="widthPencil" type="range" min="1" max="100" step="1" title="width for pencil tool." />');
                         }
                         function addSliderForLineWidth(options) {
                             var div = $('<div></div>').attr('id', options.id).addClass('menu-item');
@@ -547,7 +554,104 @@
                             });
                     }
                 }
-            }
+            },
+            Ring: {
+                CONSTANTS: {
+                    id: 'RingTool', selectionId: '#RingTool', class: 'main-tool',
+                    title: 'Click to draw ring. Click again to disable.'
+                },
+                VARIABLES: { innerRadius: 10, outerRadius: 20 },
+                start: function (options) {
+                    var event = options.event || CONSTANTS.Events.mouseclick,
+                        canvasId = '#' + (options.canvasId || CONSTANTS.canvasId),
+                        mouseOptions = null,
+                        X = null,
+                        Y = null,
+                        radius = null,
+                        innerRadius = null,
+                        outerRadius = null;
+
+                    $(canvasId).on(event, function (e) {
+                        mouseOptions = { event: e, relativeTo: $(this) };
+                        X = Actions.Mouse.getX(mouseOptions);
+                        Y = Actions.Mouse.getY(mouseOptions);
+                        innerRadius = Tools.Ring.VARIABLES.innerRadius;
+                        outerRadius = Tools.Ring.VARIABLES.outerRadius;
+                        CANVASAPI.fillRing({ X: X, Y: Y, innerRadius: innerRadius, outerRadius: outerRadius });
+                    });
+                },
+                stop: function (options) {
+                    var event = options.event || CONSTANTS.Events.mouseclick,
+                        canvasId = '#' + (options.canvasId || CONSTANTS.canvasId);
+
+                    $(canvasId).off(event);
+                },
+                ContextMenu: {
+                    activate: function (options) {
+                        function initialSlider(id, title) {
+                            return $('<input id="' + id + '" type="range" min="1" max="200" step="1" title="' + title + '" />');
+                        }
+                        function addSliderForRadius(options) {
+                            var div = $('<div></div>').attr('id', options.id).addClass('menu-item');
+                            var innerSlider = initialSlider("innerRadiusRing", "inner radius for ring tool.")
+                                            .attr('value', Tools.Ring.VARIABLES.innerRadius)
+                                            .on('mouseover', function () {
+                                                $(this).attr('title', $(this).val());
+                                            })
+                                            .on('input', function () {
+                                                Tools.Ring.VARIABLES.innerRadius = $(this).val();
+                                            });
+                            var outerSlider = initialSlider("outerRadiusRing", "outer radius for ring tool.")
+                                            .attr('value', Tools.Ring.VARIABLES.outerRadius)
+                                            .on('mouseover', function () {
+                                                $(this).attr('title', $(this).val());
+                                            })
+                                            .on('input', function () {
+                                                Tools.Ring.VARIABLES.outerRadius = $(this).val();
+                                            });
+                            outerSlider.appendTo(div);
+                            innerSlider.appendTo(div);
+                            div.appendTo($(options.containerSelectionCriterion));
+                        }
+                        addSliderForRadius(options);
+                    },
+                    deactivate: function (options) {
+                        function removeSliderForRadius(options) {
+                            $('#' + options.id).remove();
+                        }
+                        removeSliderForRadius(options);
+                    },
+                    getOptions: function () {
+                        return {
+                            tool: this,
+                            id: 'RingContextMenu',
+                            containerSelectionCriterion: '.contextual-tool-bar'
+                        };
+                    }
+                },
+                Events: {
+                    register: function (options) {
+                        var toolId = options.toolId || Tools.Ring.CONSTANTS.selectionId,
+                                     tool = $(toolId),
+                                     contextMenu = Tools.Ring.ContextMenu;
+
+                        setupToolTips(tool, Tools.Ring.CONSTANTS.title);
+                        options.tool = tool;
+
+                        tool.funcToggle('click',
+                          function () {
+                              activateTool(options);
+                              contextMenu.activate(contextMenu.getOptions());
+                              activeTool = tool;
+                          },
+                          function () {
+                              activeTool = null;
+                              deactivateTool(options);
+                              contextMenu.deactivate(contextMenu.getOptions());
+                          });
+                    }
+                }
+            },
         };
 
         var
@@ -622,7 +726,7 @@
               context.fillStyle = resetCanvasColor;
               context.fillRect(0, 0, canvasWidth, canvasHeight);
               context.restore();
-              
+
           });
       },
 
@@ -664,6 +768,13 @@
               canvasId: CONSTANTS.canvasId,
               start: Tools.PickColor.start,
               stop: Tools.PickColor.stop
+          });
+          Tools.Ring.Events.register({
+              toolId: Tools.Ring.CONSTANTS.selectionId,
+              event: CONSTANTS.Events.mouseclick,
+              canvasId: CONSTANTS.canvasId,
+              start: Tools.Ring.start,
+              stop: Tools.Ring.stop
           });
           registerAllColorsPickerEvents({ toolId: 'allColorsPicker', containerId: 'HTML5ColorPicker' });
           registerSaveImageEvents({ toolId: 'save-as-image', containerId: 'SaveImageButton' });

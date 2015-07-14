1917c3e6896cbf8524f6f665a19a685edef50df8
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-07-14 14:51:22 +0530

    Closes #124 and puts not-allowed cursor for mandelbrot sliders where cursor was none.

--- src/jspaint-common.js
+++ src/jspaint-common.js
@@ -89,7 +89,13 @@ $(function () {
             div2.appendTo(IContainBasicColors);
         },
         hexToRgb: function (hex) {
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            var result = null;
+            var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+            hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+                return r + r + g + g + b + b;
+            });
+            result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
             return result ? {
                 r: parseInt(result[1], 16),
                 g: parseInt(result[2], 16),

076d04db0d1041b15c4086a8541c59f9c115f78f
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-10-07 01:19:19 +0530

    Closes #177.

--- src/jspaint-Tools.js
+++ src/jspaint-Tools.js
@@ -2130,7 +2130,7 @@ $(function() {
         context.putImageData(image, 0, 0);
     });
 
-    $('#AddNoise').on('click', function() {
+    $('#AddNoiseTool').on('click', function() {
         var canvasId = '#' + CONSTANTS.canvasId,
             height = $(canvasId).height(),
             width = $(canvasId).width(),
@@ -2140,6 +2140,32 @@ $(function() {
             image.data[i] += Math.random() < 0.5 ? Math.random() * 255 * -1 : Math.random() * 255;
             image.data[i + 1] += Math.random() < 0.5 ? Math.random() * 255 * -1 : Math.random() * 255;
             image.data[i + 2] += Math.random() < 0.5 ? Math.random() * 255 * -1 : Math.random() * 255;
+        }
+        context.putImageData(image, 0, 0);
+    });
+
+    $('#RandomColorTool').on('click', function() {
+        var canvasId = '#' + CONSTANTS.canvasId,
+            height = $(canvasId).height(),
+            width = $(canvasId).width(),
+            image = context.getImageData(0, 0, width, height),
+            sampleX = Math.floor(Math.random() * width),
+            sampleY = Math.floor(Math.random() * height),
+            sampleRed = image.data[sampleX * width + sampleY],
+            sampleGreen = image.data[sampleX * width + sampleY + 1],
+            sampleBlue = image.data[sampleX * width + sampleY + 2],
+            red = Math.random() < 0.5 ? Math.random() * 255 * -1 : Math.random() * 255,
+            green = Math.random() < 0.5 ? Math.random() * 255 * -1 : Math.random() * 255,
+            blue = Math.random() < 0.5 ? Math.random() * 255 * -1 : Math.random() * 255;
+
+        console.log(sampleX + ", " + sampleY);
+        console.log(sampleRed + ", " + sampleGreen + ", " + sampleBlue);
+        for (var i = 0; i < image.data.length; i += 4) {
+            if (image.data[i] === sampleRed && image.data[i + 1] === sampleGreen && image.data[i + 2] === sampleBlue) {
+                image.data[i] += red;
+                image.data[i + 1] += green;
+                image.data[i + 2] += blue;
+            }
         }
         context.putImageData(image, 0, 0);
     });

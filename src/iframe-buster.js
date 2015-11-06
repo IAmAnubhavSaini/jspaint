85e43970f0bc2f5803c4b23076bdc825ac479ede
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2015-11-06 14:02:40 +0530

    iframe buster turned into a object

--- src/iframe-buster.js
+++ src/iframe-buster.js
@@ -1,60 +1,56 @@
 (function() {
     "use strict";
 
-    function warningPrefix() {
-        return 'You are visiting jspaint in an insecure way!';
-    }
+  function IFrameBuster(){
+	  var self = this instanceof IFrameBuster ? this : Object.create(IFrameBuster.prototype);
+	  self.warningPrefix = (function() { return 'You are visiting jspaint in an insecure way!'; })();
+	  self.appHomePage = (function() { return 'http://IAmAnubhavSaini.github.io/jspaint'; })();
+	  self.alertMessage = (function() { return 'Security risk! Go to original page: ' + window.self.location.href; })();
+	  return self;
+  }
+  IFrameBuster.prototype.alertUserAboutScam = function () { alert(this.alertMessage); };
 
-    function appHomePage() {
-        return 'http://IAmAnubhavSaini.github.io/jspaint';
-    }
+  IFrameBuster.prototype.setBlockerStyle = function (blocker) {
+    blocker.style.width = '100%';
+    blocker.style.height = '100%';
+    blocker.style.minHeight = '600px';
+    blocker.style.minWidth = '800px';
+    blocker.style.lineHeight = '600px';
+    blocker.style.textAlign = 'center';
+    blocker.style.fontSize = '72px';
+    blocker.style.backgroundColor = '#FF0000';
+    blocker.style.color = '#FFFFFF';
+    blocker.style.textShadow = '2px 2px #000000';
+    blocker.style.position = 'fixed';
+    blocker.style.top = '0';
 
-    function setBlockerStyle(blocker) {
-        blocker.style.width = '100%';
-        blocker.style.height = '100%';
-        blocker.style.minHeight = '600px';
-        blocker.style.minWidth = '800px';
-        blocker.style.lineHeight = '600px';
-        blocker.style.textAlign = 'center';
-        blocker.style.fontSize = '72px';
-        blocker.style.backgroundColor = '#FF0000';
-        blocker.style.color = '#FFFFFF';
-        blocker.style.textShadow = '2px 2px #000000';
-        blocker.style.position = 'fixed';
-        blocker.style.top = '0';
+    return blocker;
+  };
 
-        return blocker;
-    }
+  IFrameBuster.prototype.createBlocker = function () {
+    var blocker = document.createElement('div');
+    blocker = this.setBlockerStyle(blocker);
+    blocker.innerHTML = this.warningPrefix + 'Please visit ' + this.appHomePage;
+    return blocker;
+  };
 
-    function createBlocker() {
-        var blocker = document.createElement('div');
+  IFrameBuster.prototype.injectBlocker = function() {
+    (document.getElementsByTagName('html'))[0].appendChild(this.createBlocker());
+  };
 
-        blocker = setBlockerStyle(blocker);
+  IFrameBuster.prototype.goToJSPaintHome = function() {
+    window.top.location.replace(window.self.location.href);
+  };
 
-        blocker.innerHTML = warningPrefix() + 'Please visit ' + appHomePage();
-
-        return blocker;
-    }
-
-    function injectBlocker() {
-        (document.getElementsByTagName('html'))[0].appendChild(createBlocker());
-    }
-
-    function alertMessage() {
-        return 'Security risk! Go to original page: ' + window.self.location.href;
-    }
-
-    function alertUserAboutScam() {
-        alert(alertMessage());
-    }
-
-    function goToJSPaintHome() {
-        window.top.location.replace(window.self.location.href);
-    }
-
+  IFrameBuster.prototype.activate = function(){
     if (window.top !== window.self) {
-        injectBlocker();
-        alertUserAboutScam();
-        goToJSPaintHome();
+      this.injectBlocker();
+      this.alertUserAboutScam();
+      this.goToJSPaintHome();
     }
+  };
+
+  var buster = new IFrameBuster();
+  buster.activate();
+
 })();

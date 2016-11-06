d747a8381e4c881bf6b6d0461f5d4178ca7628f9
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-11-06 11:12:35 +0530

    TEST - adding more tests for iframe-buster.

--- src/scripts/test-iframe-buster.js
+++ src/scripts/test-iframe-buster.js
@@ -50,6 +50,41 @@ var tests = [
     toTest: typeof (new IFrameBuster()).isActivated,
     testedFor: typeof function () { },
     description: 'typeof (new IFrameBuster()).isActivated is a function'
+  },
+  {
+    toTest: (new IFrameBuster()).isActivated(),
+    testedFor: false,
+    description: 'isActivated() returns false for newly created IFrameBuster object'
+  },
+  {
+    toTest: typeof (new IFrameBuster()).alertUserAboutScam,
+    testedFor: typeof function () { },
+    description: 'typeof (new IFrameBuster()).alertUserAboutScam is a function'
+  },
+  {
+    toTest: typeof (new IFrameBuster()).setBlockerStyle,
+    testedFor: typeof function () { },
+    description: 'typeof (new IFrameBuster()).setBlockerStyle is a function'
+  },
+  {
+    toTest: typeof (new IFrameBuster()).createBlocker,
+    testedFor: typeof function () { },
+    description: 'typeof (new IFrameBuster()).createBlocker is a function'
+  },
+  {
+    toTest: typeof (new IFrameBuster()).injectBlocker,
+    testedFor: typeof function () { },
+    description: 'typeof (new IFrameBuster()).injectBlocker is a function'
+  },
+  {
+    toTest: typeof (new IFrameBuster()).goToJSPaintHome,
+    testedFor: typeof function () { },
+    description: 'typeof (new IFrameBuster()).goToJSPaintHome is a function'
+  },
+  {
+    toTest: typeof (new IFrameBuster()).activate,
+    testedFor: typeof function () { },
+    description: 'typeof (new IFrameBuster()).activate is a function'
   }
 ];
 

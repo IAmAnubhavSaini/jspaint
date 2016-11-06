1a08eb839d9e70cd95d154b1ce42b98cab72bee9
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-11-06 11:17:56 +0530

    TEST - adding use strict and introducing matchstrike.js

---
+++ src/scripts/matchstrike.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function strike(tests) {
+  var success = 0;
+  var failure = 0;
+  tests.forEach(function (t) {
+    match(t.toTest, t.testedFor, t.description) ? success++ : failure++;
+  });
+  console.log(success, 'succeeded;', failure, 'failed!');
+}
+
+function match(a, b, desc) {
+  var out = a === b ? true : false;
+  if (console && !out) {
+    console.log('match failed: ', desc, ':', a, '(toTest) is not (testedFor)', b);
+  }
+  return out;
+}

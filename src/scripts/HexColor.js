ab589189034dfedeea366b187231508360338752
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-11-06 19:03:33 +0530

    HexColor - Adding error handling, to*String functions and tests.

--- src/scripts/HexColor.js
+++ src/scripts/HexColor.js
@@ -1,11 +1,50 @@
 'use strict';
 
 var HexColor = function (r, g, b) {
-    /* pass hex numbers or strings */
+    var err = false;
+    var errs = [];
+    if(r < 0) {
+        err = true;
+        errs.push(new RangeError('r should not be less than 0; value provided: ' + r));
+    }
+    if(r > 255) {
+        err = true;
+        errs.push(new RangeError('r should not be greater than 255; value provided: ' + r));
+    }
+    if(g < 0) {
+        err = true;
+        errs.push(new RangeError('g should not be less than 0; value provided: ' + g));
+    }
+    if(g > 255) {
+        err = true;
+        errs.push(new RangeError('g should not be greater than 255; value provided: ' + g));
+    }
+    if(b < 0) {
+        err = true;
+        errs.push(new RangeError('b should not be less than 0; value provided: ' + b));
+    }
+    if(b > 255) {
+        err = true;
+        errs.push(new RangeError('b should not be greater than 255; value provided: ' + b));
+    }
+    if(err) {
+        var errMessage = errs.reduce(function(p, c, i, a) {
+            return p.toLocaleString() + '; ' + c.toLocaleString();
+        }, '');
+        throw new Error(errMessage);
+    }
     var self = this instanceof HexColor ? this : Object.create(HexColor.prototype);
-    self.red = r;
-    self.green = g;
-    self.blue = b;
-    self.value = '#' + r + g + b;
+    self.red = r.toString(16);
+    self.green = g.toString(16);
+    self.blue = b.toString(16);
+    self.value = '#' + self.red + self.green + self.blue;
     return self;
 }
+
+HexColor.prototype.toLongColorString = function () {
+    return this.value;
+};
+
+HexColor.prototype.toShortColorString = function () {
+    return '#' + this.value.substr(1, 1) + this.value.substr(3, 1) + this.value.substr(5, 1);  
+};
--- src/scripts/HexColor.test.js
+++ src/scripts/HexColor.test.js
@@ -5,7 +5,31 @@ var tests = [
     toTest: typeof HexColor,
     testedFor: typeof function () { },
     description: 'HexColor is a function'
-  }
+  },
+  (function () {
+    var allColorCases = [];
+    for (var c = 0; c < 255; c++) {
+      var actual = (new RGBColor(c, c, c)).toHexColor();
+      var hex = c.toString(16);
+      var expected = new HexColor(hex, hex, hex).value;
+      allColorCases.push({
+        toTest: '#' + actual.red + actual.green + actual.blue,
+        testedFor: expected,
+        description: 'HexColor via RGBColor'
+      });
+    }
+    return allColorCases;
+  })(),
+  {
+    toTest: typeof (new HexColor(10, 10, 10)).toLongColorString,
+    testedFor: typeof function () { },
+    description: 'HexColor has toLongColorString as a method'
+  },
+  {
+    toTest: typeof (new HexColor(10, 10, 10)).toShortColorString,
+    testedFor: typeof function () { },
+    description: 'HexColor has toShortColorString as a method'
+  },
 ];
 
 strike(tests, 'HexColor.test.js');

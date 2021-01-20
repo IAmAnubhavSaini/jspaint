03c17b66df9b82ad94c81b2a72aa4cac0dfb2e41
Anubhav <iamanubhavsaini@gmail.com>
2021-01-21 00:46:25 +0530

    REFACTOR - Introducing color.js

---
+++ src/scripts/color.js
@@ -0,0 +1,28 @@
+const COLOR = {
+    rgb2hex(r, g, b) {
+        return [r, g, b]
+            .map(c => c.toString(16))
+            .map(c => c.toUpperCase())
+            .map(c => c.padStart(2, '0'))
+            .reduce((a, c) => a + c, '#')
+    },
+    hex2rgb(hex) {
+        return [hex || '#000']
+            .map(h => h.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (matches, r, g, b) => '' + r + r + g + g + b + b))
+            .map(h => /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(h))
+            .map(mrgb => (mrgb || [0, 0, 0, 0]).slice(1))
+            .map(rgb => rgb.map(c => parseInt(c, 16)))
+            .map(rgb => ({
+                r: rgb[0],
+                g: rgb[1],
+                b: rgb[2],
+                rgb: `rgb(${rgb.join(', ')})`,
+                rgba: `rgba(${rgb.join(', ')}, 1)`
+            }))
+            .pop()
+    }
+}
+
+if (module) {
+    module.exports = COLOR
+}
--- src/scripts/jspaint-pre.js
+++ src/scripts/jspaint-pre.js
@@ -1,26 +1,7 @@
-var RGBToHex = function (r, g, b) {
-    function componentToHex(c) {
-        var hex = c.toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
-    }
-
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-};
-
-var HexToRGB = function (hex) {
-    var result = null;
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-    result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-};
+/* has dependencies on:
+ * color.js
+ *
+ */
 
 CONSTANTS = {
     canvasId: "jspaint-canvas",
@@ -211,8 +192,8 @@ var pre = $(function () {
             div1.appendTo(IContainBasicColors);
             div2.appendTo(IContainBasicColors);
         },
-        hexToRgb: HexToRGB,
-        rgbToHex: RGBToHex
+        hexToRgb: COLOR.hex2rgb,
+        rgbToHex: COLOR.rgb2hex
     };
 
     setupToolTips = function (tool, title) {

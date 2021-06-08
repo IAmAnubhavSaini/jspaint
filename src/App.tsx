37ccd218802e577f269f6d16ceef411a47d4ed40
Anubhav <iamanubhavsaini+git@gmail.com>
2021-06-08 18:58:54 +0530

    v2021.2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header>
+                <h1>Welcome to jspaint!</h1>
+            </header>
+            <main className="app-content">
+
+            </main>
+            <footer>
+                v2021.2.0
+            </footer>
+        </div>
+    );
 }
 
 export default App;

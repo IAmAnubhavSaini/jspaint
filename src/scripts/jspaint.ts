62635e6c57d66e3d0494f481c45ac245cc969ae0
Anubhav <iamanubhavsaini+git@gmail.com>
2022-01-26 00:29:09 +0530

    update design a bit

--- src/scripts/jspaint.ts
+++ src/scripts/jspaint.ts
@@ -3,7 +3,6 @@ let RGBToHex = function (r: number, g: number, b: number): string {
         let hex = c.toString(16);
         return hex.length == 1 ? "0" + hex : hex;
     }
-
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 };
 

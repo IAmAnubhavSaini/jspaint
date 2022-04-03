953ec0b4291fd473a9c62b6eb2ca05fe54e7251a
f0c1s <iamanubhavsaini+git@gmail.com>
2022-04-03 10:43:19 +0530

    clean slate

--- README.md
+++
@@ -1,9 +0,0 @@
-# jspaint
-
-Paint application in your browser. version: 0.3.0
-
-## License
-
-GPL v3.0
-
-jspaint Copyright (C) 2022 Anubhav Saini
--- tests/iframe-buster.spec.js
+++
@@ -1,5 +0,0 @@
-describe('iFrameBuster should be activated by now;', function () {
-    it('but since we are not in the browser iframe activationStatus should be false.', function () {
-        expect(iFrameBuster.isActivated()).toBe(false);
-    });
-});

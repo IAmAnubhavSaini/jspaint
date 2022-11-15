d51e6f7c0fdad820eef3871ea010d0e4c6c385f0
f0c1s <iamanubhavsaini+git@gmail.com>
2022-11-15 14:56:15 +0530

    wip

---
+++ client-react/src/App.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { Topbar } from "./features/topbar/Topbar";
+
+function App() {
+    return (
+        <div className="App">
+            <Topbar />
+            <header className="App-header">
+                <Counter />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <span>
+                    <span>Learn </span>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React
+                    </a>
+                    <span>, </span>
+                    <a
+                        className="App-link"
+                        href="https://redux.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Redux
+                    </a>
+                    <span>, </span>
+                    <a
+                        className="App-link"
+                        href="https://redux-toolkit.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Redux Toolkit
+                    </a>
+                    ,<span> and </span>
+                    <a
+                        className="App-link"
+                        href="https://react-redux.js.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React Redux
+                    </a>
+                </span>
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ client-react/src/features/topbar/Topbar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Topbar() {
+    return <div className={"flex"}>topbar</div>;
+}
